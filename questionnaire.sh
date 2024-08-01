9c03ddbcac84132b19389064ab0eeeeea2113213
Puleng Maphiri <maphiripn@gmail.com>
2024-08-01 21:16:33 +0000

    Save progress: 490.1

--- questionnaire.sh
+++ questionnaire.sh
@@ -2,18 +2,17 @@
 
 echo -e "\n~~ Questionnaire ~~\n"
 
-
 QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
+
 echo $QUESTION1
 read NAME
 
-QUESTION2="Where are you from?"
 echo $QUESTION2
 read LOCATION
 
-QUESTION3="What's your favorite coding website?"
 echo $QUESTION3
 read WEBSITE
+
 echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
-
-
